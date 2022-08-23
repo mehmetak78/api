34b2f42d318244f1535f7e83ff5444661cc530b3
mehmetak <mehmetak78@hotmail.com>
2022-08-23 21:02:04 +0300

    init

--- app.js
+++ app.js
@@ -6,6 +6,13 @@ const feedRoutes = require('./routes/feed')
 const app = express();
 app.use(express.json());
 
+app.use((req, res, next)=> {      // For CROS
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+})
+
 app.use('/feed', feedRoutes);
 
 app.listen(8080);
